a3916885bf8aa6920435260df998fae25913437f
root <root@web2.expertsmediagroup.net>
2016-06-30 14:16:40 +0000

    Update Commit

--- library/notification.js
+++ library/notification.js
@@ -1,6 +1,5 @@
 $(document).on('device-initialize',function() {
   alert = function(text) {
-    navigator.notification.alert(text,function(){},"Notification","OK");
+    navigator.notification.alert(text,function(){},'Notification','OK');
   }
 });
-
